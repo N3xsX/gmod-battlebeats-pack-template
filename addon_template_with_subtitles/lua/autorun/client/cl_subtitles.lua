9dc1131121e34341cc2927a752e4f213205811d5
N3xsX <155646867+N3xsX@users.noreply.github.com>
2025-11-02 18:07:16 +0100

    Add files via upload

---
+++ addon_template_with_subtitles/lua/autorun/client/cl_subtitles.lua
@@ -0,0 +1,43 @@
+BATTLEBEATS.subtitles = BATTLEBEATS.subtitles or {}
+
+BATTLEBEATS.subtitles["song name"] = { -- Change this to your song name! (lowercase only!)
+    raw = [[
+
+    ]]
+}
+
+
+
+
+--[=[
+
+Paste your SRT file contents (you can open the .srt file with Notepad).
+It should look like this:
+
+BATTLEBEATS.subtitles["my song"] = {
+    raw = [[
+1
+00:00:54,731 --> 00:00:59,122
+Immortal temptation
+
+2
+00:00:59,122 --> 00:01:03,088
+Takes over my mind
+
+3
+00:01:03,088 --> 00:01:07,958
+Condemned
+
+4
+00:01:07,958 --> 00:01:12,066
+Falling weak on my knees
+    ]]
+}
+
+
+
+
+
+
+
+]=]
